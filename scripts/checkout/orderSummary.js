854604ae0f49242ae570ccc3f8426fb0779ba34a
krist <kristjan.laanet@gmail.com>
2025-10-06 16:34:37 +0300

    make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,9 +1,9 @@
 import {cart, removeFromCart, updateDeliveryOption} from "../../data/cart.js";
-import {products, getProduct} from "../../data/products.js";
+import {getProduct} from "../../data/products.js";
 import {formatCurrency} from "../utils/money.js";
 import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
 import {deliveryOptions, getDeliveryOption} from "../../data/deliveryOptions.js";
-
+import {renderPaymentSummary} from "./paymentSummary.js";
 
 export function renderOrderSummary() {
 
@@ -119,6 +119,8 @@ export function renderOrderSummary() {
                 `.js-cart-item-container-${productId}`);
 
             container.remove();
+
+            renderPaymentSummary();
         });
     });
 
@@ -128,6 +130,7 @@ export function renderOrderSummary() {
                 const {productId, deliveryOptionId} = element.dataset;
                 updateDeliveryOption(productId, deliveryOptionId);
                 renderOrderSummary();
+                renderPaymentSummary();
             })
         });
 }
