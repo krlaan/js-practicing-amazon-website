85565efbced08cdaa68076a4ad704a7c7e149c1e
krist <kristjan.laanet@gmail.com>
2025-10-09 12:16:26 +0300

    create oop version of cart

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -14,8 +14,7 @@ export function renderPaymentSummary() {
         const deliveryOption = getDeliveryOption(cartItem.deliveryOptionId);
         shippingPriceCents += deliveryOption.priceCents;
     })
-    console.log(productPriceCents)
-    console.log(shippingPriceCents);
+
     const totalBeforeTaxCents = productPriceCents + shippingPriceCents;
     const taxCents = totalBeforeTaxCents * 0.1;
     const total = totalBeforeTaxCents + taxCents;
