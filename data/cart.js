8029564d4d1b1cf4e7a533ee3259d13228f64d6a
krist <krlaan@taltech.ee>
2025-10-10 12:53:26 +0300

    learn promises and covert codde to promises

--- data/cart.js
+++ data/cart.js
@@ -1,3 +1,5 @@
+
+
 export let cart;
 
 loadFormStorage();
@@ -70,4 +72,17 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
     matchingItem.deliveryOptionId = deliveryOptionId;
 
     saveToStorage();
+}
+
+export function loadCart(fun) {
+    const xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("load", () => {
+        console.log(xhr.response)
+
+        fun();
+    })
+
+    xhr.open("GET", "https://supersimplebackend.dev/cart");
+    xhr.send();
 }
